1f5a8c821875b07638a1bd35b74a37253e273e1e
Adi-Nami <nezukohage@gmail.com>
2026-05-29 00:27:19 +0530

    pwa remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,34 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'StayEasy',
-        short_name: 'StayEasy',
-        description: 'StayEasy Application',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
+    react()
   ],
   server:{
     proxy:{
